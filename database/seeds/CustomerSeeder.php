f5df2663afd0932f667de23b7be2455019585f66
pietroscandola <pietro.scandolavr@gmail.com>
2022-06-07 10:56:20 +0200

    first commit

---
+++ database/seeds/CustomerSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Customer;
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $customer = new Customer();
+            $customer->name = $faker->name();
+            $customer->surname = $faker->word();
+            $customer->phone_number = $faker->numerify('###-#######');
+            $customer->email = $faker->safeEmail();
+            $customer->save();
+        }
+    }
+}
